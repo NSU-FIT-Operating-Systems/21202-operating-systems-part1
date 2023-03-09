1dc5516611276ef3fff3db5f126ae5f6869ac77f
Maxim Golubev <maxim.golubeff2014@gmail.com>
2023-03-09 13:14:03 +0700

    corrections

--- golubevm/lab1/4/hello.c
+++ golubevm/lab1/4/hello.c
@@ -1,32 +1,31 @@
+#include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
 
+int main(int argc, char *argv[]) {
+  printf("Hello, world!\n");
 
-int main(int argc, char* argv[]){
-	printf("Hello, world!\n");
-	
-	void* handle;
-	void (*hello_dyn)(void);
-	char* error;
-	
-	handle = dlopen("libhello-dyn-run.so", RTLD_LAZY);
-	
-	if (!handle) {
-        fprintf(stderr, "Error: %s\n", dlerror());
-		dlclose(handle);
-        return EXIT_FAILURE;
-    }
-	
-	dlerror();
-	hello_dyn = (void (*)(void)) dlsym(handle, "hello_from_dyn_runtime_lib");
-	error = dlerror();
-	
-	if (error != NULL) {
-		fprintf(stderr, "Error: %s\n", error);
-		dlclose(handle);
-		return EXIT_FAILURE;
-	}
-	hello_dyn();	
-	dlclose(handle);
+  void *handle;
+  void (*hello_dyn)(void);
+  char *error;
+
+  handle = dlopen("libhello-dyn-run.so", RTLD_LAZY);
+
+  if (!handle) {
+    fprintf(stderr, "Error: %s\n", dlerror());
+    dlclose(handle);
+    return EXIT_FAILURE;
+  }
+
+  dlerror();
+  hello_dyn = (void (*)(void))dlsym(handle, "hello_from_dyn_runtime_lib");
+  error = dlerror();
+
+  if (error != NULL) {
+    fprintf(stderr, "Error: %s\n", error);
+    dlclose(handle);
+    return EXIT_FAILURE;
+  }
+  hello_dyn();
+  dlclose(handle);
 }
