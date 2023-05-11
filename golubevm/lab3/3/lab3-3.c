bf09ec60d3146564b8a75c1922576daa48063150
Maxim Golubev <maxim.golubeff2014@gmail.com>
2023-05-11 15:23:06 +0700

    lab3 fixes. Removed task3

--- golubevm/lab3/3/lab3-3.c
+++ golubevm/lab3/3/lab3-3.c
@@ -44,7 +44,7 @@ int pagemap_get_entry(PagemapEntry *entry, int pagemap_fd, uintptr_t vaddr)
 }
 
 int main(int argc, char* argv[]) {
-	if (argc < 2) {
+	if (argc < 3) {
 		fprintf(stderr, "Not enough arguments. Required: [pid] [vaddr]");
 		return -1;
 	}
