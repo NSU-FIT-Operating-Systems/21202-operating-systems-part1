89ba070c55459ecec553aa71ff9320e7a49cf3da
Maxim Golubev <maxim.golubeff2014@gmail.com>
2023-05-16 14:00:11 +0700

    heap-recursive.c fixes

--- golubevm/lab4/2/heap-recursive.c
+++ golubevm/lab4/2/heap-recursive.c
@@ -4,17 +4,20 @@
 #include <malloc.h>
 
 #define SIZE 1024*1024
+#define DELAY_BEFORE_START_PROGRAM 15
+#define RECURSION_DELAY_IN_MICS 10000
 
 void incr_heap() {
 	char *buffer = (char *)malloc(SIZE);
-	usleep(10000L);
+	usleep(RECURSION_DELAY_IN_MICS);
 	incr_heap();
+	free(buffer);
 }
 
 int main(int argc, char const *argv[])
 {
 	printf("pid: %d\n", getpid());
-	sleep(15);
+	sleep(DELAY_BEFORE_START_PROGRAM);
 	incr_heap();
 	return 0;
 }
