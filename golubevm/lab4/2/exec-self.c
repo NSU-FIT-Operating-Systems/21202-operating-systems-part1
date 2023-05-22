98a11dbac27e7ff9b5743daf40c9359fbf66b691
Maxim Golubev <maxim.golubeff2014@gmail.com>
2023-05-22 21:38:06 +0700

    add check on result of mmap, add munmap task

--- golubevm/lab4/2/exec-self.c
+++ golubevm/lab4/2/exec-self.c
@@ -2,9 +2,12 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define DELAY 5
+
 int main(int argc, char const *argv[])
 {
-	sleep(1);
+	printf("Hello, world! I'm %d\n", getpid());
+	sleep(DELAY);
 	execv(argv[0], argv);
 	return 0;
 }
--- golubevm/lab4/2/mmap-function.c
+++ golubevm/lab4/2/mmap-function.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define PAGE_SIZE 4096
 #define PAGES 10
@@ -18,6 +19,7 @@ void change_mapped_memory_protection_to_none(char *m_begin);
 void try_to_read_from_protected_memory(char *mem);
 void try_to_write_to_protected_memory(char *mem);
 void sigsegv_handler(int signo);
+void unmap_pages(char *m_begin, int length);
 
 int main(int argc, char const *argv[])
 {
@@ -29,10 +31,12 @@ int main(int argc, char const *argv[])
 	change_mapped_memory_protection_to_none(mapped_area);
 	// sleep(SLEEP);
 
-	signal(SIGSEGV, sigsegv_handler);
+	struct sigaction sa;
+	sa.sa_handler = &sigsegv_handler;
+    sigaction(SIGSEGV, &sa, NULL);
 	// try_to_read_from_protected_memory(mapped_area + 10);
-	try_to_write_to_protected_memory(mapped_area + 5);
-
+	// try_to_write_to_protected_memory(mapped_area + 5);
+	unmap_pages(mapped_area + PAGE_SIZE * 4, PAGE_SIZE * 2);
 	return 0;
 }
 
@@ -42,7 +46,11 @@ void get_pid() {
 }
 
 char *get_mapped_area() {
-	char *ptr = mmap(NULL, PAGE_SIZE * PAGES, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	void *ptr = mmap(NULL, PAGE_SIZE * PAGES, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	if (ptr == MAP_FAILED) {
+		perror("Error occured while mapping region");
+		exit(EXIT_FAILURE);
+	}
 	printf("mapped area: %p\n", ptr);
 	return ptr;
 }
@@ -50,8 +58,8 @@ char *get_mapped_area() {
 void change_mapped_memory_protection_to_none(char *m_begin) {
 	int err = mprotect(m_begin, PAGE_SIZE, PROT_NONE);
 	if (err != 0) {
-		perror(strerror(errno));
-		exit(1);
+		perror("Error occured while changing permission.");
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -64,6 +72,17 @@ void try_to_write_to_protected_memory(char *mem) {
 }
 
 void sigsegv_handler(int signo) {
-	printf("Segmenatation fault occured.\n");
-	signal(signo, SIG_IGN);
+	const char* msg = "Segmentation fault got caught by custom handler\n";
+	write(2, msg, strlen(msg));
+	exit(EXIT_FAILURE);
+}
+
+void unmap_pages(char *m_begin, int length) {
+	int res = munmap(m_begin, length);
+	if (res != 0) {
+		char msg[128];
+		sprintf(msg, "Error occured while unmapping pages at: %p with length=%d", m_begin, length);
+		perror(msg);
+		exit(EXIT_FAILURE);
+	}
 }
