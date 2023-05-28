a9cbae8e34c640c69903a246a5ba1811f948c330
EBochkarev <e.bochkarev@g.nsu.ru>
2023-05-28 16:16:41 +0700

    data failure notification, SUCCESS exit

--- Bochkarev/lab6/client.c
+++ Bochkarev/lab6/client.c
@@ -13,57 +13,69 @@
 
 int main() 
 {
-    int clSocket = 0;
-    if ((clSocket = socket(AF_INET, SOCK_DGRAM, 0)) == ERROR) 
-    {
-        perror("can't create socket for client: "); 
-        exit(EXIT_FAILURE);  
-    }
+	int clSocket = 0;
+	if ((clSocket = socket(AF_INET, SOCK_DGRAM, 0)) == ERROR)
+	{
+		perror("can't create socket for client: ");
+		exit(EXIT_FAILURE);
+	}
 
-    struct sockaddr_in sAddr;
-    sAddr.sin_family = AF_INET;
-    sAddr.sin_port = htons(PORT);
-    sAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	struct sockaddr_in sAddr;
+	sAddr.sin_family = AF_INET;
+	sAddr.sin_port = htons(PORT);
+	sAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    printf("Enter your message:\n");
+	printf("Enter your message: (enter 'exit' to end transmission)\n");
 
-    char *buf = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	char *buf = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	if (buf == NULL )
+	{
+		close(clSocket);
+		perror("mem alloc faulure: ");
+		exit(EXIT_FAILURE);
+	}
 
-    if (buf == NULL) 
-    {
-        close(clSocket);
-        perror("mem alloc faulure: "); 
-        exit(EXIT_FAILURE);
-    }
+	char *orig = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	if (orig == NULL )
+	{
+		close(clSocket);
+		perror("mem alloc faulure: ");
+		exit(EXIT_FAILURE);
+	}
 
-    int readCount = 0;
+	int readCount = 0;
 
-    while (1) 
-    {
-        fgets(buf, BUFFER_SIZE, stdin);
-        if (sendto(clSocket, buf, strlen(buf), 0, (struct sockaddr *) &sAddr, sizeof(sAddr)) == ERROR) 
-        {
-            close(clSocket);
-            free(buf);
-            perror("send faulure: "); 
-            exit(EXIT_FAILURE);
-        }
-
-        readCount = recvfrom(clSocket, buf, BUFFER_SIZE, 0, NULL, NULL);
-        if (readCount == ERROR) 
-        {
-            close(clSocket);
-            free(buf);
-            perror("recieve faulure: "); 
-            exit(EXIT_FAILURE);
-        }
-        buf[readCount] = '\0';
-
-        printf("Received: %s\n", buf);
-    }
-
-    close(clSocket);
-    free(buf);
-
-    return EXIT_SUCCESS;
+	while (1) 
+	{
+		fgets(buf, BUFFER_SIZE, stdin);
+		strcpy(orig, buf);
+		if(strcmp(buf, "exit\n") == 0)
+		{
+			break;
+		}
+		if (sendto(clSocket, buf, strlen(buf), 0, (struct sockaddr *) &sAddr, sizeof(sAddr)) == ERROR)
+		{
+			close(clSocket);
+			free(buf);
+			perror("send faulure: ");
+			exit(EXIT_FAILURE);
+		}
+		readCount = recvfrom(clSocket, buf, BUFFER_SIZE, 0, NULL, NULL);
+		if (readCount == ERROR)
+		{
+			close(clSocket);
+			free(buf);
+			perror("recieve faulure: ");
+			exit(EXIT_FAILURE);
+		}
+		buf[readCount] = '\0';
+		printf("Received: %s\n", buf);
+		if(strcmp(buf, orig) != 0)
+		{
+			printf("Sorry, your data was not sent properly\n");
+		}
+	}
+	close(clSocket);
+	free(buf);
+	return EXIT_SUCCESS;
 }
--- Bochkarev/lab6/server.c
+++ Bochkarev/lab6/server.c
@@ -10,68 +10,69 @@
 #define BUFFER_SIZE 512
 #define PORT 2222
 
-int main() 
+int main()
 {
-    int sSocket = 0;
-    if ((sSocket = socket(AF_INET, SOCK_DGRAM, 0)) == ERROR) 
-    {
-        perror("socket failed: "); 
-    	exit(EXIT_FAILURE);  
-    }
+	int sSocket = 0;
+	if ((sSocket = socket(AF_INET, SOCK_DGRAM, 0)) == ERROR)
+	{
+		perror("socket failed: ");
+		exit(EXIT_FAILURE);
+	}
 
-    struct sockaddr_in sAddr;
-    sAddr.sin_family = AF_INET;
-    sAddr.sin_port = htons(PORT);
-    sAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	struct sockaddr_in sAddr;
+	sAddr.sin_family = AF_INET;
+	sAddr.sin_port = htons(PORT);
+	sAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (bind(sSocket, (struct sockaddr *) &sAddr, sizeof(sAddr)) == ERROR) 
-    {
-        close(sSocket);
-        perror("binding error: "); 
-    	exit(EXIT_FAILURE);  
-    }
+	if (bind(sSocket, (struct sockaddr *) &sAddr, sizeof(sAddr)) == ERROR)
+	{
+		close(sSocket);
+		perror("binding error: ");
+		exit(EXIT_FAILURE);
+	}
 
-    printf("data waiting...\n");
+	printf("data waiting...\n");
 
-    unsigned int addrLen = 0;
-    int readCount = 0;
+	unsigned int addrLen = 0;
+	int readCount = 0;
 
-    struct sockaddr_in clAddr;
+	struct sockaddr_in clAddr;
 
-    char *buf = (char *) malloc(BUFFER_SIZE * sizeof(char));
-    if (buf == NULL) 
-    {
-        close(sSocket);
-        perror("alloc error (server): "); 
-    	exit(EXIT_FAILURE);  
-    }
+	char *buf = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	if (buf == NULL)
+	{
+		close(sSocket);
+		perror("alloc error (server): "); 
+		exit(EXIT_FAILURE);
+	}
 
-    while (1) 
-    {
-        addrLen = sizeof(clAddr);
-        readCount = recvfrom(sSocket, buf, BUFFER_SIZE, 0, (struct sockaddr *) &clAddr, &addrLen);
+	while (1)
+	{
+		addrLen = sizeof(clAddr);
+		readCount = recvfrom(sSocket, buf, BUFFER_SIZE, 0, (struct sockaddr *) &clAddr, &addrLen);
 
-        if (readCount == ERROR) 
-        {
-            close(sSocket);
-            free(buf);
-            perror("recieve failure: "); 
-            exit(EXIT_FAILURE);  
-        }
-        buf[readCount] = '\0';
+		if (readCount == ERROR)
+		{
+			close(sSocket);
+			free(buf);
+			perror("recieve failure: ");
+			exit(EXIT_FAILURE);
+		}
+		buf[readCount] = '\0';
 
-        printf("Received: %s\n", buf);
+		printf("Received: %s\n", buf);
 
-        if (sendto(sSocket, buf, readCount, 0, (struct sockaddr *) &clAddr, addrLen) == ERROR) 
-        {
-            close(sSocket);
-            free(buf);
-            perror("send failure: "); 
-            exit(EXIT_FAILURE);  
-        }
-    }
+		if (sendto(sSocket, buf, readCount, 0, (struct sockaddr *) &clAddr, addrLen) == ERROR)
+		{
+			close(sSocket);
+			free(buf);
+			perror("send failure: ");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    close(sSocket);
-    free(buf);
-    return EXIT_SUCCESS;
+	close(sSocket);
+	free(buf);
+
+	return EXIT_SUCCESS;
 }
