a814b407a381dc1f3defc7cc5a829afdf2ab1a49
A-B-asdf <43109133+A-B-asdf@users.noreply.github.com>
2023-05-30 12:01:24 +0700

    biryukov lab3 (1) (#16)
    * lab3 (1, 2)
    
    * добавил проверки системных вызовов
    
    * исправил makefile
    
    ---------
    
    Co-authored-by: Александр Алексеевич Бирюков <a.biryukov2@g.nsu.ru>

---
+++ abiryukov2/3/1/reverse_copy.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+char* strrev(char* str) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+    return str;
+}
+
+void reverse_copy(const char* src_dir) {
+    // создаем целевой каталог
+    char dst_dir[strlen(src_dir) + 1];
+    strcpy(dst_dir, src_dir);
+    strrev(dst_dir);
+    mkdir(dst_dir, 0777);  // 0777 - полный доступ
+
+    // открываем исходный каталог
+    DIR* src_d = opendir(src_dir);
+    if (src_d == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+
+    // читаем и копируем файлы
+    struct dirent* dir_entry;
+    while ((dir_entry = readdir(src_d)) != NULL) {
+        // пропускаем каталоги "." и ".."
+        if (strcmp(dir_entry->d_name, ".") == 0 || strcmp(dir_entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        // формируем пути к исходному и целевому файлам
+        char src_file[strlen(src_dir) + strlen(dir_entry->d_name) + 2];
+        sprintf(src_file, "%s/%s", src_dir, dir_entry->d_name);
+        char dst_file[strlen(dst_dir) + strlen(dir_entry->d_name) + 2];
+        sprintf(dst_file, "%s/%s", dst_dir, strrev(dir_entry->d_name));
+
+        // получаем информацию о файле
+        struct stat src_stat;
+        if (stat(src_file, &src_stat) == -1) {
+            perror("stat");
+            exit(EXIT_FAILURE);
+        }
+
+        if (S_ISREG(src_stat.st_mode)) {  // ну, это файл, да?
+            FILE* src_f = fopen(src_file, "rb");
+            if (src_f == NULL) {
+                perror("fopen");
+                exit(EXIT_FAILURE);
+            }
+            FILE* dst_f = fopen(dst_file, "wb");
+            if (dst_f == NULL) {
+                perror("fopen");
+                exit(EXIT_FAILURE);
+            }
+            char buffer[src_stat.st_size];
+            size_t read_size = fread(buffer, 1, src_stat.st_size, src_f);
+            if (read_size != src_stat.st_size) {
+                perror("fread");
+                exit(EXIT_FAILURE);
+            }
+            for (int i = src_stat.st_size - 1; i >= 0; i--) {  // ну и переписваем файл, вот
+                if (fputc(buffer[i], dst_f) == EOF) {
+                    perror("fputc");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            if (fclose(src_f) != 0) {
+                perror("fclose");
+                exit(EXIT_FAILURE);
+            }
+            if (fclose(dst_f) != 0) {
+                perror("fclose");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    if (closedir(src_d) != 0) {
+        perror("closedir");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <src_dir>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    reverse_copy(argv[1]);
+
+    return 0;
+}
